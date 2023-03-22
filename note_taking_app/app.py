7786896c3458366149af4812c39c6fff15f0b43f
ayeshafarheen987 <113899070+ayeshafarheen987@users.noreply.github.com>
2023-03-22 13:19:02 +0530

    Add files via upload

--- note_taking_app/app.py
+++ note_taking_app/app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
+
 app = Flask(__name__)
+app.secret_key = 'debug_notetaking__app'
 
 
-notes = []
 @app.route('/', methods=["GET","POST"])
 def index():
+    if 'notes' not in session:
+       
+        session['notes'] = []
+    notes = session['notes']
     if request.method=='POST':
-        note = request.form.get("note")
-        notes.append(note)
+        
+        if 'Clear' in request.form:
+            session['notes'] = []
+            notes = []
+        
+        elif 'note_to_remove' in request.form:
+            note_to_remove = request.form['note_to_remove']
+            notes.remove(note_to_remove)
+            session['notes'] = notes
+            
+        else:
+            note = request.form.get('note')
+        
+            if note and note.strip():
 
+                notes.append(note.strip())
+                session['notes']=notes
+                
     return render_template("home.html", notes=notes)
 
 
